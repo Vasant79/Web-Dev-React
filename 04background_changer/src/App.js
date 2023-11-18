a3f229c3be1c904298e7ff23b527977a80a51e79
Vasant Singh Negi <Vasantnegi7@gmail.com>
2023-11-18 16:01:03 +0530

    tailwind css to color changer

--- 04background_changer/src/App.js
+++ 04background_changer/src/App.js
@@ -5,14 +5,14 @@ function App() {
   /**
    * Made an Array of colors and used map
    */
-  let color = ["red", "green", "blue", "pink", "yellow"];
+  let color = ["Red", "Green", "Blue", "Pink", "Yellow"];
   let renderOutput = color.map((eachColor) => {
     return <ColorPicker key={eachColor} color={eachColor} />;
   });
 
   return (
     <>
-      <div>{renderOutput}</div>
+      <div className="flex justify-center">{renderOutput}</div>
     </>
   );
 }
--- 04background_changer/src/Components/Resuable/ColorPicker.js
+++ 04background_changer/src/Components/Resuable/ColorPicker.js
@@ -1,14 +1,19 @@
 /**
- * Best Practice -- use PropeTypes when having prsops
+ * Best Practice -- use PropeTypes when having props
  */
 
 function ColorPicker({ color }) {
-  function handleClick(event) {
+  function handleClick() {
     document.body.style.background = color;
   }
   return (
     <>
-      <button onClick={handleClick}>{color}</button>
+      <button
+        className=" w-28 ml-2 mt-10 font-mono font-light  text-xl shadow-lg  bg-white rounded-lg border-1 border-black"
+        onClick={handleClick}
+      >
+        {color}
+      </button>
     </>
   );
 }
